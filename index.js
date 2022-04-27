61320f0f9f36fc46a75d980d0e9cf1352cdc01e9
Ross McDermott <mcdermottr@gmail.com>
2022-04-27 23:59:03 +1000

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
+var fs = require('fs');
 const { getDownloadObject } = require('./lib/utils');
 
 async function setup() {
@@ -8,21 +9,29 @@ async function setup() {
     // Get version of tool to be installed
     const version = core.getInput('version');
 
-    console.log('Version is ' + version);
-
     // Download the specific version of the tool, e.g. as a tarball/zipball
     const download = getDownloadObject(version);
-
-    console.log('download', download);
-
     const pathToTarball = await tc.downloadTool(download.url);
 
-    console.log('Path to tarball is ' + pathToTarball);
     // Extract the tarball/zipball onto host runner
     const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
     const pathToCLI = await extract(pathToTarball);
 
-    console.log('Path to CLI is ' + pathToCLI);
+    //joining path of directory 
+    var directoryPath = pathToCLI;
+    //passsing directoryPath and callback function
+    fs.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            console.log(file); 
+        });
+    });
+
 
     // Expose the tool by adding it to the PATH
     core.addPath(path.join(pathToCLI, download.binPath));
